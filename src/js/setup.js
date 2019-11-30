b8ce1fae14d285b50d4399785c1d1528b0e07e4a
Rainspistols <ffly@mail.ru>
2019-11-30 13:48:33 +0100

    finished lesson 4

--- src/js/setup.js
+++ src/js/setup.js
@@ -1,7 +1,44 @@
 'use strict';
 
 var characterSettings = document.querySelector('.setup');
-characterSettings.classList.remove('hidden');
+var characterLogoOutside = document.querySelector('.setup-open');
+var btnCloseCharacterSettings = document.querySelector('.setup-close');
+var characterSettingsUserName = document.querySelector('.setup-user-name');
+
+// listeners
+var openCharacterSettings = function() {
+  characterSettings.classList.remove('hidden');
+  document.addEventListener('keydown', onCharacterSettingsEsc);
+};
+var closeCharacterSettings = function() {
+  characterSettings.classList.add('hidden');
+  document.removeEventListener('keydown', onCharacterSettingsEsc);
+};
+
+var onCharacterSettingsEsc = function(evt) {
+  if (evt.keyCode === 27) {
+    closeCharacterSettings();
+  }
+};
+var onCharacterSettingsUserName = function(evt) {
+  evt.stopPropagation();
+  if (evt.keyCode === 27) {
+    characterSettingsUserName.blur();
+  }
+};
+
+characterLogoOutside.addEventListener('click', openCharacterSettings);
+characterLogoOutside.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 13) {
+    openCharacterSettings();
+  }
+});
+btnCloseCharacterSettings.addEventListener('click', closeCharacterSettings);
+characterSettingsUserName.addEventListener(
+  'keydown',
+  onCharacterSettingsUserName
+);
+
 document.querySelector('.setup-similar').classList.remove('hidden');
 var wizards = [];
 var WIZARD_SURNAMES = [
@@ -33,11 +70,20 @@ var WIZARD_COAT_COLOR = [
   'rgb(0, 0, 0)',
 ];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIREBALL_COLOR = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848',
+];
 
+// get random number
 function getRandomIndex(itemNum) {
   return Math.floor(Math.random() * itemNum);
 }
 
+// build wizzards
 var buildWizards = function() {
   for (let i = 0; i < 4; i++) {
     var randomIndexNames = getRandomIndex(WIZARD_NAMES.length),
@@ -46,27 +92,50 @@ var buildWizards = function() {
       randomIndexEyes = getRandomIndex(WIZARD_EYES_COLOR.length);
     let obj = {};
     obj['name'] =
-      WIZARD_SURNAMES[randomIndexSurnames] + ' ' + WIZARD_NAMES[randomIndexNames];
+      WIZARD_SURNAMES[randomIndexSurnames] +
+      ' ' +
+      WIZARD_NAMES[randomIndexNames];
     obj['coatColor'] = WIZARD_COAT_COLOR[randomIndexCoat];
     obj['eyesColor'] = WIZARD_EYES_COLOR[randomIndexEyes];
     wizards.push(obj);
   }
-}
+};
 buildWizards();
 
 var charactersList = document.querySelector('.setup-similar-list');
 var charactersListTemplate = document
   .querySelector('#similar-wizard-template')
   .content.querySelector('.setup-similar-item');
-var wizardEyes = document.querySelector('#wizard-eyes');
+var wizardEyes = document.querySelector('.wizard-eyes');
 
 for (var i = 0; i < 4; i++) {
   var wizardUnit = charactersListTemplate.cloneNode(true);
 
   wizardUnit.querySelector('.setup-similar-label').textContent =
     wizards[i].name;
-    wizardUnit.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-    wizardUnit.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+  wizardUnit.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardUnit.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
 
   charactersList.appendChild(wizardUnit);
 }
+
+// change mage items color
+var wizardCoat = document.querySelectorAll('.wizard-coat');
+var fireball = document.querySelector('.setup-fireball');
+var fireballBlock = document.querySelector('.setup-fireball-wrap');
+var fireballInput = fireballBlock.querySelector('input');
+document.addEventListener('click', function(evt) {
+  if (evt.target.classList.contains('wizard-coat')) {
+    evt.target.style.fill = getRandomItemColor(WIZARD_COAT_COLOR);
+  } else if (evt.target.classList.contains('wizard-eyes')) {
+    evt.target.style.fill = getRandomItemColor(WIZARD_EYES_COLOR);
+  } else if (evt.target.classList.contains('setup-fireball')) {
+    fireballBlock.style.background = getRandomItemColor(WIZARD_FIREBALL_COLOR);
+    fireballInput.value = fireballBlock.style.background;
+  }
+  console.log(fireballInput);
+});
+
+var getRandomItemColor = function(itemName) {
+  return itemName[getRandomIndex(itemName.length)];
+};
