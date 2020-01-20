76d0b55a13e462e4dba9f66419ce12cea80c9291
Rainspistols <ffly@mail.ru>
2020-01-20 19:38:33 +0100

    replace mocked data by serverData, create post and get request with error system

--- src/js/backend.js
+++ src/js/backend.js
@@ -1,46 +1,29 @@
 'use strict';
 
 (function() {
-  function onLoad(name, data, cb) {
-    window.backend[name] = data;
-    console.log('success 200!');
-    cb();
-  }
-
-  function onError(message) {
-    console.error(message);
-  }
-
-  function getNewFormData(form) {
-    return new FormData(form);
-  }
-  function onLoadPost(e) {
-    console.log(e.target);
-    e.preventDefault();
-  }
-
-  var form = document.querySelector('.setup-wizard-form');
-  form.addEventListener('submit', (e) => {
-    window.backend.save(
-      getNewFormData(form),
-      'https://js.dump.academy/code-and-magick',
-      onLoadPost,
-      onError
-    );
-  });
+  // function onLoad
 
   window.backend = {
-    load(name, url, onLoadCb, onErrorCb) {
+
+    onLoadGet(data, cb) {
+      window.backend.wizardsData = data;
+      console.log('success 200!');
+      cb();
+    },
+    onError(message) {
+      console.error(message);
+    },
+    load(onLoad, onError) {
       var xhr = new XMLHttpRequest();
       xhr.responseType = 'json';
       xhr.timeout = 2000;
-      xhr.open('GET', url);
+      xhr.open('GET', 'https://js.dump.academy/code-and-magick/data');
       xhr.send();
       var error;
       xhr.onload = () => {
         switch (xhr.status) {
           case 200:
-            onLoadCb(name, xhr.response, window.buildWizards);
+            onLoad(xhr.response, window.buildWizards);
             break;
           case 400:
             error = 'Invalid request';
@@ -52,24 +35,25 @@
             error = 'Nothing found';
             break;
           default:
-            error = 'Response Status: ' + name.status + ' ' + name.statusText;
+            error = 'Response Status: ' + xhr.status + ' ' + xhr.statusText;
         }
         if (error) {
-          onErrorCb(error);
+          onError(error);
         }
       };
     },
-    save(data, url, onLoadCb, onErrorCb) {
+    save(data, onLoadPost, onError) {
       var xhr = new XMLHttpRequest();
       xhr.responseType = 'json';
       xhr.timeout = 2000;
-      xhr.open('POST', url);
+      xhr.open('POST', 'https://js.dump.academy/code-and-agick');
+      console.log(data);
       xhr.send(data);
       var error;
       xhr.onload = () => {
         switch (xhr.status) {
           case 200:
-            onLoadCb(e);
+            onLoadPost();
             break;
           case 400:
             error = 'Invalid request';
@@ -81,19 +65,14 @@
             error = 'Nothing found';
             break;
           default:
-            error = 'Response Status: ' + name.status + ' ' + name.statusText;
+            error = 'Response Status: ' + xhr.status + ' ' + xhr.statusText;
         }
         if (error) {
-          onErrorCb(error);
+          onError(error);
         }
       };
     },
   };
 
-  window.backend.load(
-    'wizardsData',
-    'https://js.dump.academy/code-and-magick/data',
-    onLoad,
-    onError
-  );
+  window.backend.load(window.backend.onLoadGet, window.backend.onError);
 })();
